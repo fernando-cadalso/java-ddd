5587239a7a19fbbf31fce2fa3e8ff86f21c28d9f
Fernando de Alcântara Cadalso <fernando.cadalsoii@gmail.com>
2021-09-04 18:36:54 -0300

    Adição do ouvinte GerarSeloParaAlunoMatriculado na aplicação.

--- src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
+++ src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,9 +1,11 @@
-package br.com.alura.escola.academico;
+package br.com.alura.escola;
 
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
 import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
 import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.gamificacao.aplicacao.GerarSeloParaAlunoMatriculado;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
 import br.com.alura.escola.shared.dominio.evento.PublicadorDeEvento;
 
 public class MatricularAlunoPorLinhaDeComando {
@@ -15,6 +17,7 @@ public class MatricularAlunoPorLinhaDeComando {
 	
 		PublicadorDeEvento publicador = new PublicadorDeEvento();
 		publicador.adicionar(new LogDeAlunoMatriculado());
+		publicador.adicionar(new GerarSeloParaAlunoMatriculado(new RepositorioDeSelosEmMemoria()));
 		MatricularAluno matricular = new MatricularAluno(new RepositorioDeAlunosEmMemoria(), publicador);
 		matricular.executa(
 				new MatricularAlunoDto(nome, 
